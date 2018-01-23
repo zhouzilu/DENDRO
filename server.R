6802f09e90b2d5d86b74a587422a6ab09059cf4f
zhouzilu <zhouzilu@pennmedicine.upenn.edu>
2018-01-23 15:53:07 -0500

    update-shinning app
    - include more parameters
    - include finer grid
    - add illustration on statistics
    - higher number of permutation to stablize statistics

--- server.R
+++ server.R
@@ -13,11 +13,14 @@ dend=as.dendrogram(hclust(dist(rbind(c(1,0,0),c(0,1,0),c(0,1,1)),method='binary'
 labels(dend)=c('a','b','c')
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  load('Lratiodist_eval_fp.rda')
-  kprob3=c(0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
-  lprob3=c(0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9)
+  # load('C:/Users/zhouzilu/Desktop/research/single cell/SNV simulation/hpc_res/appSNV_Lratiodist_eval_all.rda')
+  load('appSNV_Lratiodist_eval_all.rda')
+  kprob3=c(0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.98)
+  lprob3=c(0.01,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.98)
   fprate_v=c(0,0.2,0.4,0.6,0.8)
-  n_v=c(100,1000,2000,5000,10000)
+  n_v=c(50,100,150,200,250,300,350,400,500,1000,1500,2000,5000,10000)
+  rd_v=c(10,20,30,40,50,60,70,80,90,100)
+  m_v=c(50,100,130)
   output$distPlot <- renderPlot({
     # generate kprob and lprob based on input from ui.R
     f = which.min(abs(fprate_v-input$fp/100))
@@ -37,26 +40,37 @@ shinyServer(function(input, output) {
     
     n = which.min(abs(n_v-input$n))
     
-    ari = round(eval_final[k,l,f,n,1],2)
+    rd = which.min(abs(rd_v-input$r))
+    
+    m = which.min(abs(m_v-input$m))
+    
+    ari = round(eval_final[k,l,f,n,rd,m,1],2)
+    sen = round(eval_final[k,l,f,n,rd,m,2],2)
+    spe = round(eval_final[k,l,f,n,rd,m,3],2)
+    det = round(eval_final[k,l,f,n,rd,m,4],2)
     # draw the dendrogram with the specified kprob and lprob
+    par(mar=c(7.1,0.1,0.1,1.1),cex=1.5)
     dend %>% set("branches_k_color", value = c("red", "blue",'green'), k = 3) %>%
       set("leaves_pch", c(17, 18, 19)) %>%  # node point type
       set("leaves_cex", 2) %>%  # node point size
       set("leaves_col", c("blue", "red", "green")) %>%
-      plot(main = "Customized colors",ylim=c(-0.5,1.1))
-    text(1.1,0.5,lprobj)
-    text(2.1,0.25,lprobj)
-    text(3.1,0.25,lprob)
-    text(2.6,0.75,lprobj)
-    text(1.7,1.1,fp)
-    text(1,-0.1,kprobi)
-    text(2,-0.1,kprobi)
-    text(3,-0.1,kprob)
-    text(2.5,-0.25, paste0('Total number of mutations are ',n))
-    text(2.5,-0.5, paste0('Adjusted Random Index: ',ari))
-    
-  })
-  
+      plot(main = NULL,yaxt='n',ylim=c(-1,1.1),cex=1.5)
+    par(cex=1)
+    text(1.1,0.5,lprobj,cex=1.5)
+    text(2.1,0.25,lprobj,cex=1.5)
+    text(3.1,0.25,paste0('SMP\n',lprob),cex=1.5)
+    text(2.6,0.75,lprobj,cex=1.5)
+    text(1.7,1.1,paste0('fp ',fp),cex=1.5)
+    text(3.1,1.1,paste0('Rd ',rd_v[rd]),cex=1.5)
+    text(1,-0.25,kprobi,cex=1.5)
+    text(2,-0.25,kprobi,cex=1.5)
+    text(3,-0.20,paste0('SCP\n',kprob),cex=1.5)
+    text(2,-0.45, paste0('Total number of mutations are ',n_v[n]),cex=1.5)
+    text(2,-0.60, paste0('Total number of cells are ',m_v[m]),cex=1.5)
+    text(2,-0.75, paste0('Detection probability: ',det),cex=1.5)
+    text(2,-0.90, paste0('Sensitivity: ',sen,'    Specificity: ',spe),cex=1.5)
+    text(2,-1.05, paste0('Adjusted Rand Index: ',ari),cex=1.5)
+  },height=600)
 })
 
 #  ------------------------------------------------------------------------
